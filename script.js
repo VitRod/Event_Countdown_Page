a86960e920967116b19b1bd6a30ee8b059543037
Vitaliy Rodyna <ukrdao@gmail.com>
2022-10-18 07:00:31 +0300

    const countdownEl = document.getElementById('countdown'); const countdownElTitle = document.getElementById('countdown-title'); const countdownBtn = document.getElementById('countdown-button'); const timeElements = document.querySelectorAll('span');

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
 const countdownForm = document.getElementById('countdownForm');
 const inputContainer = document.getElementById('input-container');
 const dateEl = document.getElementById('date-picker');
+
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
