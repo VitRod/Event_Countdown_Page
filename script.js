aed2a10f0ff83518c9b845f7fa5b9718e1458110
Vitaliy Rodyna <ukrdao@gmail.com>
2022-10-19 07:20:00 +0300

    add

--- script.js
+++ script.js
@@ -18,35 +18,34 @@ const modalShow = document.getElementById('show-modal');
 const modalClose = document.getElementById('close-modal');
 
 const suggestedListNodeList = document.getElementsByClassName('suggested-list-li');
-
 for (let i = 0; i < suggestedListNodeList.length; i++) {
-    suggestedListNodeList[i].addEventListener('click', (e) => {
-      modal.classList.remove('show-modal');
-      let value = e.target.innerHTML;
-      localStorage.removeItem('countdown');
-      countdownTitle = '';
-      countdownDate = '';
-      updateCountdown(value, suggestedList[value]);
-    });
-  }
+  suggestedListNodeList[i].addEventListener('click', (e) => {
+    modal.classList.remove('show-modal');
+    let value = e.target.innerHTML;
+    localStorage.removeItem('countdown');
+    countdownTitle = '';
+    countdownDate = '';
+    updateCountdown(value, suggestedList[value]);
+  });
+}
 
-  const suggestedList = {
-    'Presidential Election': 1604361600000,
-    Christmas: 1608854400000,
-    Hanukkah: 1607558400000,
-    Thanksgiving: 1606348800000,
-    'Yom Kippur': 1601164800000,
-    "Valentine's Day": 1613260800000,
-    Halloween: 1604102400000,
-    'Friday the 13th': 1605225600000,
-    'Black Friday': 1606435200000,
-    "New Year's Eve": 1609372800000,
-  };
+const suggestedList = {
+  'Presidential Election': 1604361600000,
+  Christmas: 1608854400000,
+  Hanukkah: 1607558400000,
+  Thanksgiving: 1606348800000,
+  'Yom Kippur': 1601164800000,
+  "Valentine's Day": 1613260800000,
+  Halloween: 1604102400000,
+  'Friday the 13th': 1605225600000,
+  'Black Friday': 1606435200000,
+  "New Year's Eve": 1609372800000,
+};
 
 // Show Modal, Focus on Input
 function showModal() {
-    modal.classList.add('show-modal');
-  }
+  modal.classList.add('show-modal');
+}
 
 // Modal Event Listeners
 modalShow.addEventListener('click', showModal);
@@ -102,7 +101,6 @@ const updateDOM = () => {
   }, second);
 };
 
-
 const updateCountdown = (e = undefined, suggestedTitle, suggestedValue) => {
   if (typeof e === 'object') {
     e.preventDefault();
@@ -156,7 +154,6 @@ const restorePreviousCountdown = () => {
     updateDOM();
   }
 };
-
 const displayCountdown = (countdownTitle, countdownDate, countdownValue) => {
   const countdownElement = document.createElement('div');
   countdownElement.classList.add('countdown');
@@ -203,11 +200,3 @@ completeBtn.addEventListener('click', reset);
 
 // On Load, check localStorage
 restorePreviousCountdown();
-
-
-
-
-
-
-
-
